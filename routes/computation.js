6b26ac8f59ca2902cb848598dc914bccadd4bac1
Vaishnavi130820 <143207526+Vaishnavi130820@users.noreply.github.com>
2023-10-25 21:53:08 -0500

    added render

--- routes/computation.js
+++ routes/computation.js
@@ -2,16 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+let x;
+let y;
 router.get('/', function(req, res, next) {
-  const x = req.query.x ? parseFloat(req.query.x) : Math.random(); // Use query value or generate a random value
+  
+if(req.query.x ==undefined ) {
+  x=Math.floor(Math.random()*10);
+  y=Math.fround(Math.random());
+let y1=Math.imul(x,y);
+let y2=Math.log(y);
+let y3=Math.log10(x);
+  res.send('Math.imul() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.log() applied to '+y+' is '+y2+"<br>"+'Math.log10()  applied to '+x+' is'+y3); 
 
-  // Calculate the result using Math.asin()
-  const result = Math.asin (x);
+}
+else{
 
-  // Prepare the response string
-  const responseString = `${Math.asin.name} applied to ${x} is ${result}`;
+  x = req.query.x;
+  let y1=Math.imul(x,x);
+  let y2=Math.log(x);
+  let y3=Math.log10(x);
 
-  res.send(responseString);
+  res.send('Math.imul() applied to '+x+' and '+y+' is ' +y1+"<br>" +'Math.log() applied to '+y+' is '+y2+"<br>"+'Math.log10()  applied to '+x+' is'+y3);
+}
 });
 
 module.exports = router;
